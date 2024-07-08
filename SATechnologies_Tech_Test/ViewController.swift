9c468131f65740bf352ac6cb21dc65e24a7055dc
Shravan Agrawal <shravan393@gmail.com>
2024-07-08 12:23:33 +0530

    Initial Commit

---
+++ SATechnologies_Tech_Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SATechnologies_Tech_Test
+//
+//  Created by Shravan Agrawal on 08/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
